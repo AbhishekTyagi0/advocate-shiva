ad00c8b0f4a4fe40d52dac95df67f2567e61072f
AbhishekTyagi0 <abhishektyagigi4310@gmail.com>
2024-06-15 17:31:13 +0530

    added the analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import dynamic from "next/dynamic";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,8 @@ export const metadata: Metadata = {
     "Experienced legal counsel in Surajpur Court,Gautam Budh Nagar, India. Specialized in corporate law, real estate, and litigation. Committed to providing effective legal solutions.",
 };
 
-const Navbar = dynamic(() => import("@/app/components/Navbar"), { ssr: false });
-const Footer = dynamic(() => import("@/app/components/Footer"), { ssr: false });
+const Navbar = dynamic(() => import("@/app/components/Navbar"));
+const Footer = dynamic(() => import("@/app/components/Footer"));
 
 export default function RootLayout({
   children,
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
